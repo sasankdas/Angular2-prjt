9a7acdde41411a7be2c63ee52dd94b1c1580f657
sasankdas <alladisasankdas@gmail.com>
2018-01-27 13:30:28 -0500

    Created recipe list and recipe detail pages with components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+
+    new Recipe("A testrecipe", "this is simply a test", "http://goodtoknow.media.ipcdigital.co.uk/111/000016ef4/543a_orh412w625/Butter-chicken-recipe.jpg"),
+    ];
   constructor() { }
 
   ngOnInit() {
