8fb98141fca47f783a7aae7205bb55d6f6aec5a1
sasankdas <alladisasankdas@gmail.com>
2018-02-01 15:22:24 -0500

    cross component communication using services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import {Recipe} from "./recipe.model";
+import { EventEmitter } from "@angular/core";
 
 export class RecipeService{
+recipeSelected = new EventEmitter<Recipe>();
+
   private  recipes: Recipe[] = [
 
         new Recipe("Malai Koftha", "Best Malai Koftha ever", "http://goodtoknow.media.ipcdigital.co.uk/111/000016ef4/543a_orh412w625/Butter-chicken-recipe.jpg"),
