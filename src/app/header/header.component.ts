a208816bc67beec471995208ccbcac768a4bfbb4
sasankdas <alladisasankdas@gmail.com>
2018-02-04 14:34:30 -0500

    adding navigation to app and marking active routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -7,8 +7,5 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent{
-@Output() featureSelected = new EventEmitter<string>();
 
-onSelect(feature:string){
-this.featureSelected.emit(feature)}
- }; 
+}
