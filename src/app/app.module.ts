d1803bf6979e755f6beee15f575fdb549c3c0d77
sasankdas <alladisasankdas@gmail.com>
2018-02-05 11:49:17 -0500

    Added navigation to edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {DropdownDirective} from "./shared/dropdown.directive";
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 @NgModule({
@@ -29,6 +30,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingEditComponent,
     DropdownDirective,
     RecipeStartComponent,
+    RecipeEditComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from "../recipe.model";
 import {RecipeService} from "../recipe.service";
-import { ActivatedRoute , Params } from '@angular/router';
+import { ActivatedRoute , Params, Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -11,7 +13,7 @@ import { ActivatedRoute , Params } from '@angular/router';
 export class RecipeDetailComponent implements OnInit {
 recipe : Recipe;
 id : number ;
-  constructor(private recipeService : RecipeService, private route : ActivatedRoute) { }
+  constructor(private recipeService : RecipeService, private route : ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
   this.route.params.subscribe((params : Params) =>{this.id = +params["id"] ; 
@@ -19,5 +21,8 @@ id : number ;
   }
   onAddToShoppingList(){
 this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+  onEditRecipe(){
+this.router.navigate(["edit"], {relativeTo:this.route})
   }
 }
