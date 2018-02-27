21a026d5dcc2edb7cb51995a9292f12efe15ecdb
sasankdas <alladisasankdas@gmail.com>
2018-02-27 13:18:25 -0500

    started authentication. Created a signUp page and Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+import { SignupComponent } from "./auth/signup/signup.component";
 
 
 
@@ -18,6 +19,7 @@ const appRoutes : Routes = [
     {path : ":id/edit" , component : RecipeEditComponent}],
     },
     {path : "shopping-list" , component : ShoppingListComponent},
+    {path : 'signup' , component : SignupComponent}
    
 
 ];
