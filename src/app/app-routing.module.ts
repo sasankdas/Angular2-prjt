ac9c7cb9e8151aa9fbe6a3c28a52219f83b406d6
sasankdas <alladisasankdas@gmail.com>
2018-02-28 10:44:14 -0500

    signIN users with there email and password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.compon
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { SignupComponent } from "./auth/signup/signup.component";
+import { SigninComponent } from "./auth/signin/signin.component";
 
 
 
@@ -19,7 +20,8 @@ const appRoutes : Routes = [
     {path : ":id/edit" , component : RecipeEditComponent}],
     },
     {path : "shopping-list" , component : ShoppingListComponent},
-    {path : 'signup' , component : SignupComponent}
+    {path : 'signup' , component : SignupComponent},
+    {path : 'signin' , component : SigninComponent}
    
 
 ];
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,4 +5,13 @@ export class AuthService{
     signupUser(email:string, password:string){
 firebase.auth().createUserWithEmailAndPassword(email,password).catch(error =>console.log(error));
     }
+
+
+signinUser(email:string, password:string){
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+}
+
+
 }
