436a6e5e5b46155123237952c0dfa15d059b2397
sasankdas <alladisasankdas@gmail.com>
2018-02-28 11:30:00 -0500

    sending the token for save data and fetch data to occur after login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,15 +2,25 @@ import * as firebase from 'firebase';
 
 
 export class AuthService{
+token : string;
+
+
     signupUser(email:string, password:string){
 firebase.auth().createUserWithEmailAndPassword(email,password).catch(error =>console.log(error));
     }
 
 
 signinUser(email:string, password:string){
-    firebase.auth().signInWithEmailAndPassword(email,password)
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
+    firebase.auth().signInWithEmailAndPassword(email,password).catch(error => console.log(error))
+    .then(response => {firebase.auth().currentUser.getToken()
+        .then((token:string) => this.token = token)});
+    }
+
+getToken(){
+    firebase.auth().currentUser.getToken()
+    .then((token:string) => this.token = token);
+    return this.token;
+
 }
 
 
