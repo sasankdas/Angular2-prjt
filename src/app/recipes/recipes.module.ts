913aba4768de3e1276eee1633382dd4f28c9a37b
sasankdas <alladisasankdas@gmail.com>
2018-03-01 12:09:11 -0500

    Created a Shared Module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -8,6 +8,7 @@ import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.compo
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
     declarations: [
@@ -23,6 +24,7 @@ imports: [
     CommonModule,
     ReactiveFormsModule,
     RecipesRoutingModule,
+    SharedModule
 ]
 
 
