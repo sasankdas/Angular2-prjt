a3598847cab58558060514ab815e506806613c28
sasankdas <alladisasankdas@gmail.com>
2018-03-01 11:41:49 -0500

    Registered Routes in the RecipeFeature module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -7,6 +7,7 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
 @NgModule({
     declarations: [
@@ -20,7 +21,8 @@ import { CommonModule } from "@angular/common";
 
 imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RecipesRoutingModule,
 ]
 
 
